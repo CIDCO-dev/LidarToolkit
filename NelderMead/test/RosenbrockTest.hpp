c37fdcb530a716c11eb4b4d5befa7143ee2c5e14
JordanMcManus <jordan.mcmanus@cidco.ca>
2022-01-06 10:57:55 -0500

    fminserach c++ NerderMead

---
+++ NelderMead/test/RosenbrockTest.hpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2022 © Centre Interdisciplinaire de développement en Cartographie des Océans (CIDCO), Tous droits réservés
+ */
+
+/* 
+ * File:   RosenbrockTest.hpp
+ * Author: Jordan McManus
+ *
+ * Created on January 6, 2022, 9:43 AM
+ */
+
+#ifndef ROSENBROCKTEST_HPP
+#define ROSENBROCKTEST_HPP
+
+#include "catch.hpp"
+
+#include "../include/asa047.hpp"
+
+TEST_CASE("Test NelderMead function minimiser with Rosenbrock") {
+    //TODO: test asa047 algorithm on Rosenbrock function
+}
+
+
+#endif /* ROSENBROCKTEST_HPP */
+
