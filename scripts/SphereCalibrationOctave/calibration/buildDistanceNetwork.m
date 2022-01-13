13a7954e59ceaafbafa16a209a65eb3c882d379b
JordanMcManus <jordan.mcmanus@cidco.ca>
2022-01-13 14:53:56 -0500

    added distance network funciton

---
+++ scripts/SphereCalibrationOctave/calibration/buildDistanceNetwork.m
@@ -0,0 +1,12 @@
+function nodes = buildDistanceNetwork(cells, knownRadius)
+  nodes = [];
+  
+  for i = 1:size(cells)(2)
+    xyz = cells{i};
+    meanXYZ = mean(xyz);
+    s1 = xyz - meanXYZ;
+    [estimatedSphereCenter, residuals] = SphereFitFixedRadius(s1, knownRadius);
+    nodes = [nodes ; estimatedSphereCenter' + meanXYZ];
+  endfor
+  
+endfunction
